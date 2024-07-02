0ebe525d108ecb13d763499d0d090a138f1f8d57
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-07-02 08:49:58 +0530

    Update activity3_3.scala

--- activity3_3.scala
+++ activity3_3.scala
@@ -4,8 +4,8 @@ object activity3_3 {
 
   def average1(num1: Int, num2: Int): Double = {
     val avg= (num1+num2)/2.0
-    val roundedAvg = BigDecimal(avg).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-     return roundedAvg
+
+     return avg
   }
 
   def main(args: Array[String]): Unit = {
